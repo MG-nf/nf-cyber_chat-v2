51ea87494a0a82e0ccbdc13c3f9dcc28f77f4967
Marcel <marcel.grieb+neuefische@gmail.com>
2026-06-02 13:21:37 +0200

    threads module logic

--- src/threads/threads.controller.ts
+++ src/threads/threads.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { ThreadsService } from './threads.service';
+import { CreateThreadDto } from './dto/create-thread.dto';
+import { UpdateThreadDto } from './dto/update-thread.dto';
+import { Thread } from './thread.entity';
 
 @Controller('threads')
-export class ThreadsController {}
+export class ThreadsController {
+  constructor(private readonly threadsService: ThreadsService) {}
+
+  @Post()
+  create(@Body() createThreadDto: CreateThreadDto): Promise<Thread> {
+    return this.threadsService.create(createThreadDto);
+  }
+
+  @Get()
+  findAll(): Promise<Thread[]> {
+    return this.threadsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Thread> {
+    return this.threadsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateThreadDto: UpdateThreadDto,
+  ): Promise<Thread> {
+    return this.threadsService.update(id, updateThreadDto);
+  }
+}
--- src/threads/threads.service.ts
+++ src/threads/threads.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateThreadDto } from './dto/create-thread.dto';
+import { UpdateThreadDto } from './dto/update-thread.dto';
+import { Thread } from './thread.entity';
 
 @Injectable()
-export class ThreadsService {}
+export class ThreadsService {
+  constructor(
+    @InjectRepository(Thread)
+    private readonly threadsRepository: Repository<Thread>,
+  ) {}
+
+  async create(createThreadDto: CreateThreadDto): Promise<Thread> {
+    const newThread = this.threadsRepository.create(createThreadDto);
+    return await this.threadsRepository.save(newThread);
+  }
+
+  async findAll(): Promise<Thread[]> {
+    return await this.threadsRepository.find();
+  }
+
+  async findOne(id: string): Promise<Thread> {
+    const thread = await this.threadsRepository.findOne({ where: { id } });
+    if (!thread) {
+      throw new NotFoundException(`Thread with ID "${id}" not found`);
+    }
+    return thread;
+  }
+
+  async update(id: string, updateThreadDto: UpdateThreadDto): Promise<Thread> {
+    const thread = await this.findOne(id);
+
+    Object.assign(thread, updateThreadDto);
+
+    return await this.threadsRepository.save(thread);
+  }
+}
