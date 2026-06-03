efc472b0c344da5bb5868f50cb1cb9f865cdfd20
Marcel <marcel.grieb+neuefische@gmail.com>
2026-06-03 10:31:59 +0200

    fix: wiring

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,21 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from 'src/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, UsersService],
   controllers: [AuthController],
   exports: [AuthService, JwtModule],
 })
